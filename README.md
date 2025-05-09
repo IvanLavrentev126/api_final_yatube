91883a4f99ae95f58c012901857fb9f3e2d11793
Иван Лаврентьев <ivan.pavlovich.2005@mail.ru>
2025-05-09 20:41:20 +0300

    NEW_COMMIT

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# api_final
-api final
+# api_final_yatube
+## 1. Описание проекта:
+
+REST API для социальной платформы Yatube. Полнофункциональное API позволяет взаимодействовать со всеми основными сущностями платформы: публикациями, комментариями, группами и подписками.
+## 2. Как запустить проект
+Клонировать репозиторий и перейти в него в командной строке:
+
+```
+git clone git@github.com:IvanLavrentev126/api_final_yatube.git
+```
+
+```
+cd api_final_yatube
+```
+
+Cоздать и активировать виртуальное окружение:
+
+```
+python3 -m venv venv
+```
+
+```
+source venv/Scripts/activate
+```
+
+Установить зависимости из файла requirements.txt:
+
+```
+python3 -m pip install --upgrade pip
+```
+
+```
+pip install -r requirements.txt
+```
+
+Выполнить миграции:
+
+```
+python3 manage.py migrate
+```
+
+Запустить проект:
+
+```
+python3 manage.py runserver
+```
+___
+## 3. Примеры использования
+```http
+POST /api/v1/jwt/create/
+Content-Type: application/json
+
+{
+  "username": "ваш_логин",
+  "password": "ваш_пароль"
+}
+```
+### Получение публикаций
+```
+GET /api/v1/posts/
+[
+    {
+        "id": свое,
+        "author": "свое",
+        "image": null,
+        "text": "свое", #обязательное поле
+        "pub_date": "свое",
+        "group": null
+    }
+]
+```
+### Создание публикации
+```
+POST /api/v1/posts/
+{
+    "text": "свое"
+}
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,68 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets, mixins, permissions, filters
+from django.shortcuts import get_object_or_404
+from posts.models import Post, Comment, Follow, Group, User
+from .serializers import (
+    PostSerializer,
+    CommentSerializer,
+    GroupSerializer,
+    FollowSerializer
+)
+from rest_framework.pagination import LimitOffsetPagination
+from .permissions import OwnerOrReadOnly
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = (OwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (OwnerOrReadOnly,)
+
+    def post_filter(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return Comment.objects.filter(post=post)
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class CreateListViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    pass
+
+
+class FollowViewSet(CreateListViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(
+            user=self.request.user,
+            following=get_object_or_404(
+                User,
+                username=self.request.data["following"]
+            )
+        )
