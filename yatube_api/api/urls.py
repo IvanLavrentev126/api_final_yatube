f09ad7d9421790d30061d6ca4de6b64a7f611153
Иван Лаврентьев <ivan.pavlovich.2005@mail.ru>
2025-05-16 21:14:22 +0300

    16.05.2025

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,11 +4,7 @@ from .views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
 
 router = SimpleRouter()
 router.register('posts', PostViewSet, basename='posts')
-router.register(
-    r'posts/(?P<post_id>\d+)/comments',
-    CommentViewSet,
-    basename='comments'
-)
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
 router.register('groups', GroupViewSet, basename='groups')
 router.register('follow', FollowViewSet, basename='follow')
 
